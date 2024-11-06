b05509567de8866758f9561d1115d6970f1b83e8
Edgar MARTIN <edgarmartin2k18@gmail.com>
2024-11-06 15:53:24 +0100

    Ajout de la classe Evenement

---
+++ app/Models/Evenement.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use DateTime;
+
+class Evenement
+{
+    private ?int $id;
+    private ?string $titre;
+    private ?string $description;
+    private ?DateTime $date;
+    private ?string $lieu;
+    private ?bool $status;
+
+    public function __construct(?int $id, ?string $titre, ?string $description, ?DateTime $date, ?string $lieu, ?bool $status)
+    {
+        $this->id = $id;
+        $this->titre = $titre;
+        $this->description = $description;
+        $this->date = $date;
+        $this->lieu = $lieu;
+        $this->status = $status;
+    }
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function getLieu()
+    {
+        return $this->lieu;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    // Setters
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    public function setLieu($lieu)
+    {
+        $this->lieu = $lieu;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+}
