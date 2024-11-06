67cb3eeebed3caee3b8986e6825812bb24833519
Kyllian Morance <morance.kyllian@gmail.com>
2024-11-06 16:14:17 +0100

    ajout de la classe Actualite.php

---
+++ app/Models/Actualite.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use DateTime;
+
+class Actualite
+{
+    private ?int $id_actualite;
+    private ?string $type;
+    private ?string $titre;
+    private ?string $resume;
+    private ?DateTime $date_publication;
+    private ?string $image;
+
+    public function __construct(?int $id, ?string $type, ?string $titre, ?string $resume, ?DateTime $date_publication, ?string $image)
+    {
+        $this->id = $id;
+        $this->thme = $type;
+        $this->titre = $titre;
+        $this->resume = $resume;
+        $this->date_publication = $date_publication;
+        $this->image = $image;
+    }
+
+    // Getters
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getType()
+    {
+        return $this->type;
+    }
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+    public function getResume()
+    {
+        return $this->resume;
+    }
+    public function getDatePublication()
+    {
+        return $this->date_publication;
+    }
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    // Setters
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+    public function setResume($resume)
+    {
+        $this->resume = $resume;
+    }
+    public function setDatePublication($date_publication)
+    {
+        $this->date_publication = $date_publication;
+    }
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+}
