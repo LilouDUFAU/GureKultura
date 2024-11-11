6477c06fb6d000391dcc87b0dd913ca5827bec5b
Edgar MARTIN <edgarmartin2k18@gmail.com>
2024-11-11 12:05:36 +0100

    Ajout de la template de la carte Evénement

---
+++ php/carteEvt.php
@@ -0,0 +1,14 @@
+<?php
+////////////////////////////////////
+// BLOC A METTRE DANS UN CONTROLLEUR
+// Ajout du code commun à toutes les pages
+require_once 'include.php';
+
+$breadcrumbs = Breadcrumb::generate();
+
+// Rendre le template Twig
+echo $twig->render('carteEvt.html.twig', [
+    'breadcrumbs' => $breadcrumbs,
+    'title' => 'Carte des événements'
+]);
+////////////////////////////////////
