a49bbc1c495d8eab226a10dd5a6e9f8941a20a8d
Edgar MARTIN <edgarmartin2k18@gmail.com>
2024-11-11 10:53:37 +0100

    Ajout des pages de mdp oublié et code de vérification

---
+++ php/codeVerif.php
@@ -0,0 +1,15 @@
+<?php
+////////////////////////////////////
+// BLOC A METTRE DANS UN CONTROLLEUR
+// Ajout du code commun à toutes les pages
+require_once 'include.php';
+
+$breadcrumbs = Breadcrumb::generate();
+
+// Rendre le template Twig
+echo $twig->render('codeVerif.html.twig', [
+    'breadcrumbs' => $breadcrumbs,
+    'title' => 'Code de vérification'
+]);
+////////////////////////////////////
+?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,23 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-spinner': {
+          '-moz-appearance': 'textfield',           /* Firefox */
+          '-webkit-appearance': 'none',             /* Chrome, Safari, Opera */
+          '&::-webkit-inner-spin-button': {
+            '-webkit-appearance': 'none',
+            margin: '0',
+          },
+          '&::-webkit-outer-spin-button': {
+            '-webkit-appearance': 'none',
+            margin: '0',
+          },
+        },
+      });
+    },
+  ],
 }
 
