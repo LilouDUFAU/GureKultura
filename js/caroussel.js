9720056a84c20f31a74cbb2accc5948c24d73bfb
Lilou Dufau <ldufau007@iutbayonne.univ-pau.fr>
2024-11-12 11:00:37 +0100

    ajout de carroussel dans la page de calendrier de l'aviron bayonnais

---
+++ js/caroussel.js
@@ -0,0 +1,28 @@
+const carousel = document.querySelector('.carousel');
+const slides = document.querySelectorAll('.slide');
+const slidesToShow = 3; // Nombre de diapositives visibles
+const totalSlides = slides.length;
+let currentSlide = 0;
+
+document.getElementById('next').addEventListener('click', () => {
+  if (currentSlide < totalSlides - slidesToShow) {
+    currentSlide += slidesToShow;
+  } else {
+    currentSlide = 0; // Revenir au début du carrousel
+  }
+  updateCarousel();
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+  if (currentSlide > 0) {
+    currentSlide -= slidesToShow;
+  } else {
+    currentSlide = totalSlides - slidesToShow; // Aller à la fin
+  }
+  updateCarousel();
+});
+
+function updateCarousel() {
+  const slideWidth = slides[0].clientWidth; // Largeur d'une diapositive
+  carousel.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
