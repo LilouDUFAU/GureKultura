2278264f658152ad59e851e92142c1af1033fb63
Lilou Dufau <ldufau007@iutbayonne.univ-pau.fr>
2024-11-12 17:04:39 +0100

    modification des elements du carroussel

--- js/caroussel.js
+++ js/caroussel.js
@@ -1,5 +1,6 @@
 // Fonction pour initialiser un carrousel
-function initCarousel(carouselContainer, nextBtnId, prevBtnId, slidesToShow) {
+function initCarousel(carouselContainer, nextBtnId, prevBtnId) {
+  const slidesToShow = parseInt(carouselContainer.getAttribute('data-slides'), 10);
   const carousel = carouselContainer.querySelector('.carousel');
   const slides = carouselContainer.querySelectorAll('.slide');
   const totalSlides = slides.length;
@@ -32,5 +33,7 @@ function initCarousel(carouselContainer, nextBtnId, prevBtnId, slidesToShow) {
 }
 
 // Initialisation des deux carrousels
-initCarousel(document.querySelector('#carousel1'), 'next1', 'prev1', 6);
-initCarousel(document.querySelector('#carousel2'), 'next2', 'prev2', 6);
+initCarousel(document.querySelector('#carousel1'), 'next1', 'prev1');
+initCarousel(document.querySelector('#carousel2'), 'next2', 'prev2');
+initCarousel(document.querySelector('#carousel3'), 'next3', 'prev3');
+initCarousel(document.querySelector('#carousel4'), 'next4', 'prev4');
