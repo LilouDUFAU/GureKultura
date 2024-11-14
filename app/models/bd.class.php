989cc7673f4013dd024ff98e03d271da9dca6eae
Lilou Dufau <ldufau007@iutbayonne.univ-pau.fr>
2024-11-14 09:32:54 +0100

    ajout d'une ligne apres la connexion a la bd pour gerer les accents

--- app/models/bd.class.php
+++ app/models/bd.class.php
@@ -8,6 +8,7 @@ class Bd {
     public function __construct() {
         try {
             $this->pdo = new PDO('mysql:host='. DB_HOST .'; dbname='. DB_NAME, DB_USER, DB_PASS);
+            $this->pdo->exec('SET NAMES utf8mb4');
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch (PDOException $exception) {
