c5891806b523e0d6ca6150c7f29b5ac419d540d1
Maxime Etchecopar <metchecop001@iutbayonne.univ-pau.fr>
2024-11-14 09:20:29 +0100

    Ajout des événements sur la gauche de la page

--- php/reservation.php
+++ php/reservation.php
@@ -3,12 +3,17 @@
 // BLOC A METTRE DANS UN CONTROLLEUR
 // Ajout du code commun à toutes les pages
 require_once 'include.php';
+$pdo = Bd::getInstance()->getPdo();
+$managerActualite = new ActualiteDao($pdo);
+$actualite = $managerActualite->findAll();
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
 $twig = new \Twig\Environment($loader);
 
 // Rendre le template Twig pour la réservation
 echo $twig->render('reservation.html.twig', [
-    'title' => 'Réservation de Place'
+    'breadcrumbs' => $breadcrumbs,
+    'title' => 'Réservation de Place',
+    'actualites' => $actualite
 ]);
 ////////////////////////////////////
