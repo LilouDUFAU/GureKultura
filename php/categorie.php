ed1bed9213c0a30966560c9fd58e00da33312b37
Kyllian Morance <morance.kyllian@gmail.com>
2024-11-14 10:05:34 +0100

    ajout page categorie

---
+++ php/categorie.php
@@ -0,0 +1,18 @@
+<?php
+////////////////////////////////////
+// BLOC A METTRE DANS UN CONTROLLEUR
+// Ajout du code commun à toutes les pages
+require_once 'include.php';
+require_once 'prerequis.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+$twig = new \Twig\Environment($loader);
+
+
+// Rendre le template Twig
+echo $twig->render('categorie.html.twig', [
+    'breadcrumbs' => $breadcrumbs,
+    'title' => 'Categories',
+    'actualites' => $actualite
+]);
+////////////////////////////////////
