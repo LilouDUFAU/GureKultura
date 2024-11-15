f9ee9d34d8552eb77c11fe9b6f1b597ff5a4e80b
Lilou Dufau <ldufau007@iutbayonne.univ-pau.fr>
2024-11-15 21:45:13 +0100

    ajout des controllers de chaque page

---
+++ app/controllers/controller_propEv.class.php
@@ -0,0 +1,28 @@
+<?php 
+
+class ControllerPropEv extends Controller {
+
+    public function __construct(\Twig\Environment $twig, \Twig\Loader\FileSystemLoader $loader) {
+        parent::__construct($twig, $loader);
+    }
+
+    public function afficher() {
+        echo "afficher propEv";
+    }
+
+    public function lister() {
+        $pdo = Bd::getInstance()->getPdo();
+
+        $loader = new \Twig\Loader\FilesystemLoader('../templates');
+        $twig = new \Twig\Environment($loader);
+
+        $managerActualite = new ActualiteDao($this->getPdo());
+        $actualite = $managerActualite->findAll();
+
+        // Rendre le template Twig
+        echo $this->getTwig()->render('propEv.html.twig', [
+            'title' => 'Proposisition d\'événement',
+            'actualites' => $actualite
+        ]);
+    }   
+}
--- php/include.php
+++ php/include.php
@@ -18,6 +18,13 @@ require_once '../app/controllers/controller_factory.class.php';
 require_once '../app/controllers/controller_inscription.class.php';
 require_once '../app/controllers/controller_connexion.class.php';
 require_once '../app/controllers/controller_index.class.php';
+require_once "../app/controllers/controller_compte.class.php";
+require_once "../app/controllers/controller_calAvB.class.php";
+require_once "../app/controllers/controller_calBO.class.php";
+require_once "../app/controllers/controller_categorie.class.php";
+require_once "../app/controllers/controller_codeVerif.class.php";
+require_once "../app/controllers/controller_mdpOublie.class.php";
+require_once "../app/controllers/controller_propEv.class.php";
 
 //Ajout des modèles 
 require_once '../app/models/actualite.classe.php';
