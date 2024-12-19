1f9db4ce1a35e82f99ffc11f23a5bd817daf4411
Lilou Dufau <ldufau007@iutbayonne.univ-pau.fr>
2024-12-19 10:41:15 +0100

    modification de la classe evenement

--- app/models/Evenement.class.php
+++ app/models/Evenement.class.php
@@ -4,119 +4,551 @@
 
 use DateTime;
 
+
+/***
+ * @brief Classe Evenement
+ * 
+ * @details Cette classe permet de gérer les événements
+ * 
+ * @details Cette classe contient les attributs de la table Evenement
+ */
 class Evenement
 {
+
+    /***
+     * @brief ID de l'événement
+     * 
+     * @var int
+     */
     private ?int $evtId;
+
+
+    /***
+     * @brief Titre de l'événement
+     * 
+     * @var string
+     */
     private ?string $titre;
-    private ?string $descr;
-    private ?DateTime $dateEvt;
-    private ?string $loc;
-    private ?bool $statutEvt;
-    private ?string $img;
+
+
+    /***
+     * @brief Autorisation de l'événement
+     * 
+     * @var string
+     */
+    private ?string $autorisation;
+
+
+    /***
+     * @brief Email de l'événement
+     * 
+     * @var string
+     */
+    private ?string $email;
+
+
+    /***
+     * @brief Téléphone de l'événement
+     * 
+     * @var string
+     */
+    private ?string $tel;
+
+
+    /***
+     * @brief Nom du représentant
+     * 
+     * @var string
+     */
+    private ?string $nomRep;
+
+
+    /***
+     * @brief Prénom du représentant
+     * 
+     * @var string
+     */
+    private ?string $prenomRep;
+
+
+    /***
+     * @brief Description de l'événement
+     * 
+     * @var string
+     */
+    private ?string $description;
+
+
+    /***
+     * @brief Début de la date
+     * 
+     * @var DateTime
+     */
+    private ?DateTime $debutDate;
+
+
+    /***
+     * @brief Fin de la date
+     * 
+     * @var DateTime
+     */
+    private ?DateTime $finDate;
+
+
+    /***
+     * @brief Début de l'heure
+     * 
+     * @var DateTime
+     */
+    private ?DateTime $debutHeure;
+
+
+    /***
+     * @brief Fin de l'heure
+     * 
+     * @var DateTime
+     */
+    private ?DateTime $finHeure;
+
+
+    /***
+     * @brief Lieu de l'événement
+     * 
+     * @var string
+     */
+    private ?string $lieu;
+
+
+    /***
+     * @brief Photo de l'événement
+     * 
+     * @var string
+     */
+    private ?string $photo; 
+     
+
+    /***
+     * @brief Catégorie de l'événement
+     * 
+     * @var Categorie
+     */
     private ?Categorie $categorie;
 
+
+    /***
+     * @brief Nom de la catégorie
+     * 
+     * @var string
+     */
     private ?string $nomCategorie;
 
-    public function __construct(?int $evtId=null, ?string $titre=null, ?string $descr=null, ?DateTime $dateEvt=null, ?string $loc=null, ?bool $statutEvt=null, ?string $img=null, ?Categorie $categorie=null)
+
+    /***
+     * @brief Constructeur de la classe Evenement
+     * 
+     * @param int $evtId, string $titre,string $autorisation, string $email, string $tel, string $nomRep, string $prenomRep, string $description, DateTime $debutDate, DateTime $finDate, DateTime $debutHeure, DateTime $finHeure, string $lieu, string $photo, Categorie $categorie, string $nomCategorie
+     * 
+     * @return void
+     */
+    public function __construct(?int $evtId=null, ?string $titre=null, ?string $autorisation=null,?string $email=null, ?string $tel=null, ?string $nomRep=null, ?string $prenomRep=null, ?string $description=null, ?DateTime $debutDate=null, ?DateTime $finDate=null, ?DateTime $debutHeure=null, ?DateTime $finHeure=null, ?string $lieu=null, ?string $photo=null, ?Categorie $categorie=null, ?string $nomCategorie=null)
     {
         $this->evtId = $evtId;
         $this->titre = $titre;
-        $this->descr = $descr;
-        $this->dateEvt = $dateEvt;
-        $this->loc = $loc;
-        $this->statutEvt = $statutEvt;
-        $this->img = $img;
+        $this->autorisation = $autorisation;
+        $this->email = $email;
+        $this->tel = $tel;
+        $this->nomRep = $nomRep;
+        $this->prenomRep = $prenomRep;
+        $this->description = $description;
+        $this->debutDate = $debutDate;
+        $this->finDate = $finDate;
+        $this->debutHeure = $debutHeure;
+        $this->finHeure = $finHeure;
+        $this->lieu = $lieu;
+        $this->photo = $photo;
         $this->categorie = $categorie;
+        $this->nomCategorie = $nomCategorie;
     }
 
     // Getters
+
+    /***
+     * @brief Getter de l'ID
+     * 
+     * @return int
+     */
     public function getEvtId(): ?int
     {
         return $this->evtId;
     }
 
+
+    /***
+     * @brief Getter du titre
+     * 
+     * @return string
+     */
     public function getTitre(): ?string
     {
         return $this->titre;
     }
 
-    public function getDescr(): ?string
+
+    /***
+     * @brief Getter de l'autorisation
+     * 
+     * @return string
+     */
+    public function getAutorisation(): ?string
     {
-        return $this->descr;
+        return $this->autorisation;
     }
 
-    public function getDateEvt(): ?DateTime
+
+    /***
+     * @brief Getter de l'email
+     * 
+     * @return string
+     */
+    public function getEmail(): ?string
     {
-        return $this->dateEvt;
+        return $this->email;
     }
 
-    public function getLoc(): ?string
+
+    /***
+     * @brief Getter du téléphone
+     * 
+     * @return string
+     */
+    public function getTel(): ?string
     {
-        return $this->loc;
+        return $this->tel;
     }
 
-    public function getStatutEvt(): ?bool
+
+    /***
+     * @brief Getter du nom du représentant
+     * 
+     * @return string
+     */
+    public function getNomRep(): ?string
     {
-        return $this->statutEvt;
+        return $this->nomRep;
     }
 
-    public function getImg(): ?string
+
+    /***
+     * @brief Getter du prénom du représentant
+     * 
+     * @return string
+     */
+    public function getPrenomRep(): ?string
     {
-        return $this->img;
+        return $this->prenomRep;
     }
 
+
+    /***
+     * @brief Getter de la description
+     * 
+     * @return string
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+
+    /***
+     * @brief Getter du début de la date
+     * 
+     * @return DateTime
+     */
+    public function getDebutDate(): ?DateTime
+    {
+        return $this->debutDate;
+    }
+
+
+    /***
+     * @brief Getter de la fin de la date
+     * 
+     * @return DateTime
+     */
+    public function getFinDate(): ?DateTime
+    {
+        return $this->finDate;
+    }
+
+
+    /***
+     * @brief Getter du début de l'heure
+     * 
+     * @return DateTime
+     */
+    public function getDebutHeure(): ?DateTime
+    {
+        return $this->debutHeure;
+    }
+
+
+    /***
+     * @brief Getter de la fin de l'heure
+     * 
+     * @return DateTime
+     */
+    public function getFinHeure(): ?DateTime
+    {
+        return $this->finHeure;
+    }
+
+
+    /***
+     * @brief Getter du lieu
+     * 
+     * @return string
+     */
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+
+    /***
+     * @brief Getter de la photo
+     * 
+     * @return string
+     */
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+
+    /***
+     * @brief Getter de la catégorie
+     * 
+     * @return Categorie
+     */
     public function getCategorie(): ?Categorie
     {
         return $this->categorie;
     }
 
 
+    /***
+     * @brief Getter du nom de la catégorie
+     * 
+     * @return string
+     */
     public function getNomCategorie(): ?string
     {
         return $this->nomCategorie;
     }
 
     // Setters
+
+
+    /***
+     * @brief Setter de l'ID
+     * 
+     * @param int $evtId
+     * 
+     * @return void
+     */
     public function setEvtId(?int $evtId): void
     {
         $this->evtId = $evtId;
     }
 
+
+    /***
+     * @brief Setter du titre
+     * 
+     * @param string $titre
+     * 
+     * @return void
+     */
     public function setTitre(?string $titre): void
     {
         $this->titre = $titre;
     }
 
-    public function setDescr(?string $descr): void
+
+    /***
+     * @brief Setter de l'autorisation
+     * 
+     * @param string $autorisation
+     * 
+     * @return void
+     */
+    public function setAutorisation(?string $autorisation): void
     {
-        $this->descr = $descr;
+        $this->autorisation = $autorisation;
     }
 
-    public function setDateEvt(?DateTime $dateEvt): void
+
+    /***
+     * @brief Setter de l'email
+     * 
+     * @param string $email
+     * 
+     * @return void
+     */
+    public function setEmail(?string $email): void
     {
-        $this->dateEvt = $dateEvt;
+        $this->email = $email;
     }
 
-    public function setLoc(?string $loc): void
+
+    /***
+     * @brief Setter du téléphone
+     * 
+     * @param string $tel
+     * 
+     * @return void
+     */
+    public function setTel(?string $tel): void
     {
-        $this->loc = $loc;
+        $this->tel = $tel;
     }
 
-    public function setStatutEvt(?bool $statutEvt): void
+
+    /***
+     * @brief Setter du nom du représentant
+     * 
+     * @param string $nomRep
+     * 
+     * @return void
+     */
+    public function setNomRep(?string $nomRep): void
     {
-        $this->statutEvt = $statutEvt;
+        $this->nomRep = $nomRep;
     }
 
-    public function setImg(?string $img): void
+
+    /***
+     * @brief Setter du prénom du représentant
+     * 
+     * @param string $prenomRep
+     * 
+     * @return void
+     */
+    public function setPrenomRep(?string $prenomRep): void
     {
-        $this->img = $img;
+        $this->prenomRep = $prenomRep;
     }
 
+
+    /***
+     * @brief Setter de la description
+     * 
+     * @param string $description
+     * 
+     * @return void
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+
+    /***
+     * @brief Setter du début de la date
+     * 
+     * @param DateTime $debutDate
+     * 
+     * @return void
+     */
+    public function setDebutDate(?DateTime $debutDate): void
+    {
+        $this->debutDate = $debutDate;
+    }
+
+
+    /***
+     * @brief Setter de la fin de la date
+     * 
+     * @param DateTime $finDate
+     * 
+     * @return void
+     */
+    public function setFinDate(?DateTime $finDate): void
+    {
+        $this->finDate = $finDate;
+    }
+
+
+    /***
+     * @brief Setter du début de l'heure
+     * 
+     * @param DateTime $debutHeure
+     * 
+     * @return void
+     */
+    public function setDebutHeure(?DateTime $debutHeure): void
+    {
+        $this->debutHeure = $debutHeure;
+    }
+
+
+    /***
+     * @brief Setter de la fin de l'heure
+     * 
+     * @param DateTime $finHeure
+     * 
+     * @return void
+     */
+    public function setFinHeure(?DateTime $finHeure): void
+    {
+        $this->finHeure = $finHeure;
+    }
+
+
+    /***
+     * @brief Setter du lieu
+     * 
+     * @param string $lieu
+     * 
+     * @return void
+     */
+    public function setLieu(?string $lieu): void
+    {
+        $this->lieu = $lieu;
+    }
+
+    /***
+     * @brief Setter de la photo
+     * 
+     * @param string $photo
+     * 
+     * @return void
+     */
+    public function setPhoto(?string $photo): void
+    {
+        $this->photo = $photo;
+    }
+
+    /***
+     * @brief Setter de la catégorie
+     * 
+     * @param Categorie $categorie
+     * 
+     * @return void
+     */
     public function setCategorie(?Categorie $categorie): void
     {
         $this->categorie = $categorie;
     }
 
+    /***
+     * @brief Setter du nom de la catégorie
+     * 
+     * @param string $nomCategorie
+     * 
+     * @return void
+     */
     public function setNomCategorie(?string $nomCategorie): void
     {
         $this->nomCategorie = $nomCategorie;
