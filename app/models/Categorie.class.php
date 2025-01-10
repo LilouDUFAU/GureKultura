295eb964c1c5cfb994a96913cfd19cdb03d70ba8
Julian Campistron <julian.campistron@gmail.com>
2025-01-10 18:48:02 +0100

    Ajout de commentaires Doxygen sur la class et le DAO de Categorie

--- app/models/Categorie.class.php
+++ app/models/Categorie.class.php
@@ -2,13 +2,46 @@
 
 // namespace App\Models;
 
+/**
+ * @brief Classe Categorie
+ * @details Cette classe permet de gérer les catégories
+ */
 class Categorie 
 {
+    /**
+     * @brief cateId
+     * @details Identifiant de la catégorie
+     */
     private ?int $cateId;
+
+    /**
+     * @brief nom
+     * @details nom de la catégorie
+     */
     private ?string $nom;
+
+    /**
+     * @brief categorieOriginale
+     * @details Catégorie originale de la catégorie (si elle en possède une)
+     */
     private ?int $categorieOriginale;
+
+    /**
+     * @brief img
+     * @details Image représentant la catégorie
+     */
     private ?string $img;
 
+    /**
+     * @constructor
+     * @brief Constructeur de la classe Categorie
+     * @details Ce constructeur permet de créer une nouvelle catégorie
+     * @param int $cateId
+     * @param string $nom
+     * @param int $categorieOriginale
+     * @param string $img
+     * @return void
+     */
     public function __construct(?int $cateId=null, ?string $nom=null, ?int $categorieOriginale=null, ?string $img=null)
     {
         $this->cateId = $cateId;
@@ -17,45 +50,101 @@ class Categorie
         $this->img = $img;
     }
 
-    //getters
+    /////////////////////////////
+    ////////// Getters //////////
+    /////////////////////////////
 
+    /**
+     * @function getCateId
+     * @brief Getter de l'ID
+     * @details Cette fonction permet de récupérer l'identifiant de la catégorie
+     * @return int
+     */
     public function getCateId(): ?int
     {
         return $this->cateId;
     }
 
+    /**
+     * @function getNom
+     * @brief Getter du nom
+     * @details Cette fonction permet de récupérer le nom de la catégorie
+     * @return int
+     */
     public function getNom(): ?string
     {
         return $this->nom;
     }   
 
+    /**
+     * @function getCategorieOriginale
+     * @brief Getter de la catégorie originale
+     * @details Cette fonction permet de récupérer la catégorie originale de la catégorie
+     * @return int
+     */
     public function getCategorieOriginale(): ?int
     {
         return $this->categorieOriginale;
     }
 
+    /**
+     * @function getImg
+     * @brief Getter de l'image
+     * @details Cette fonction permet de récupérer l'image de la catégorie
+     * @return int
+     */
     public function getImg(): ?string
     {
         return $this->img;
     }
 
-    //setters
+    /////////////////////////////
+    ////////// Setters //////////
+    /////////////////////////////
 
+    /**
+     * @function setCateId
+     * @brief Setter de l'identifiant
+     * @details Cette fonction permet de définir l'identifiant de la catégorie
+     * @param int $cateId
+     * @return void
+     */
     public function setCateId(?int $cateId): void
     {
         $this->cateId = $cateId;
     }
 
+    /**
+     * @function setNom
+     * @brief Setter du nom
+     * @details Cette fonction permet de définir le nom de la catégorie
+     * @param string $nom
+     * @return void
+     */
     public function setNom(?string $nom): void
     {
         $this->nom = $nom;
     }
 
+    /**
+     * @function setCategorieOriginale
+     * @brief Setter de la catégorie originale
+     * @details Cette fonction permet de définir la catégorie originale de la catégorie
+     * @param int $categorieOriginale
+     * @return void
+     */
     public function setCategorieOriginale(?int $categorieOriginale): void
     {
         $this->categorieOriginale = $categorieOriginale;
     }
 
+    /**
+     * @function setImg
+     * @brief Setter de l'image
+     * @details Cette fonction permet de définir l'image de la catégorie
+     * @param string $img
+     * @return void
+     */
     public function setImg(?string $img): void
     {
         $this->img = $img;
--- app/models/categorie.dao.php
+++ app/models/categorie.dao.php
@@ -1,21 +1,55 @@
 <?php 
 
+/**
+ * @brief Classe CategorieDao, permettant d'effectuer des requêtes sur la table categorie de la base de données.
+ * @details Cette classe utilise le pattern DAO (Data Access Object).
+ */
 class CategorieDao {
 
+    /**
+     * @brief Attribut permettant de stocker la connexion à la base de données
+     * @details Cet attribut est privé et ne peut être modifié que par les méthodes de la classe
+     */
     private ?PDO $pdo;
 
+    /**
+     * @constructor CategorieDao
+     * @brief Constructeur de la classe EvenementDao
+     * @details Permet d'instancier un objet EvenementDao
+     * @return void
+     */
     public function __construct(?PDO $pdo=null) {
         $this->pdo = $pdo;
     }
 
+    /**
+     * @function getPDO
+     * @brief Getter de l'attribut pdo
+     * @details Permet de récupérer la connexion à la base de données
+     * @return PDO
+     */
     public function getPdo(): ?PDO {
         return $this->pdo;
     }   
 
+    /**
+     * @function setPDO
+     * @brief Setter de l'attribut pdo
+     * @details Permet de modifier la connexion à la base de données        
+     * @param PDO $pdo
+     * @return void
+     */
     public function setPdo(?PDO $pdo): void {
         $this->pdo = $pdo;
     }
 
+    /**
+     * @function find
+     * @brief Fonction permettant de récupérer une catégorie en base de données
+     * @details Cette fonction permet de récupérer une catégorie en base de données en fonction de son identifiant
+     * @param int $id
+     * @return Categorie
+     */
     public function find(?int $id): ?Categorie {
         $sql="SELECT * FROM " . PREFIX_TABLE . "categorie WHERE cateId = :id";
         $pdoStatement = $this->pdo->prepare($sql);
@@ -26,6 +60,12 @@ class CategorieDao {
         return $categorie;
     }
 
+    /**
+     * @function findAll
+     * @brief Fonction permettant de récupérer toutes les catégories en base de données
+     * @details Cette fonction permet de récupérer toutes les catégories en base de données
+     * @return array
+     */
     public function findAll() {
         $sql="SELECT * FROM " . PREFIX_TABLE . "categorie";
         $pdoStatement = $this->pdo->prepare($sql);
@@ -36,6 +76,12 @@ class CategorieDao {
         return $categorie;
     }
 
+    /**
+     * @function findEvtSport
+     * @brief Fonction permettant de récupérer les catégories d'événements sportifs en base de données
+     * @details Cette fonction permet de récupérer les catégories d'événements sportifs en base de données
+     * @return array
+     */
     public function findEvtSport() {
         $sql="SELECT DISTINCT(gk_evenement.cateId), gk_categorie.nom, gk_categorie.cateId, gk_categorie.categorieOriginale, gk_categorie.img 
         FROM gk_evenement 
@@ -48,6 +94,13 @@ class CategorieDao {
         $categorie = $this->hydrateAll($CategorieTab);
         return $categorie;
     }
+
+    /**
+     * @function findEvtCult
+     * @brief Fonction permettant de récupérer les catégories d'événements culturels en base de données
+     * @details Cette fonction permet de récupérer les catégories d'événements culturels en base de données
+     * @return array
+     */
     public function findEvtCult() {
         $sql="SELECT DISTINCT(gk_evenement.cateId), gk_categorie.nom, gk_categorie.cateId, gk_categorie.categorieOriginale, gk_categorie.img 
         FROM gk_evenement 
@@ -61,6 +114,12 @@ class CategorieDao {
         return $categorie;
     }
 
+    /**
+     * @function findActuSport
+     * @brief Fonction permettant de récupérer les catégories d'actualités sportives en base de données
+     * @details Cette fonction permet de récupérer les catégories d'actualités sportives en base de données
+     * @return array
+     */
     public function findActuSport() {
         $sql="SELECT DISTINCT(gk_actualite.cateId), gk_categorie.nom, gk_categorie.cateId, gk_categorie.categorieOriginale, gk_categorie.img
         FROM gk_actualite 
@@ -73,6 +132,13 @@ class CategorieDao {
         $categorie = $this->hydrateAll($CategorieTab);
         return $categorie;
     }
+
+    /**
+     * @function findActuCult
+     * @brief Fonction permettant de récupérer les catégories d'actualités culturelles en base de données
+     * @details Cette fonction permet de récupérer les catégories d'actualités culturelles en base de données
+     * @return array
+     */
     public function findActuCult() {
         $sql="SELECT DISTINCT(gk_actualite.cateId), gk_categorie.nom, gk_categorie.cateId, gk_categorie.categorieOriginale, gk_categorie.img
         FROM gk_actualite 
@@ -86,6 +152,13 @@ class CategorieDao {
         return $categorie;
     }
 
+    /**
+     * @function hydrate
+     * @brief Fonction permettant d'hydrater une catégorie en base de données
+     * @details Cette fonction permet d'hydrater une catégorie en base de données
+     * @param int|null $id
+     * @return Evenement|null
+     */
     public function hydrate(array $tab): Categorie {
         $categorie = new Categorie();
         $categorie->setCateId($tab['cateId']);
@@ -95,6 +168,13 @@ class CategorieDao {
         return $categorie;
     }
 
+    /**
+     * function hydrateAll
+     * @brief Fonction permettant d'hydrater un tableau de données en objets Categorie
+     * @details Cette fonction permet d'hydrater toutes les catégories
+     * @param array $tab
+     * @return array
+     */
     public function hydrateAll(array $tab): array {
         $categories = [];
         foreach ($tab as $categorie) {
@@ -103,7 +183,12 @@ class CategorieDao {
         return $categories;
     } 
 
-    // recupere tout des categories appartenant a la categorie originale sport pour chaque evenement (pour la page categorie)
+    /**
+     * function findCateOriSportEvt
+     * @brief Fonction permettant de récupérer tout des catégories appartenant à la catégorie originale sport pour chaque evenement
+     * @details Cette fonction permet de récupérer tout des catégories appartenant à la catégorie originale sport pour chaque evenement
+     * @return array
+     */
     public function findCateOriSportEvt(): array {
         $stmt = $this->pdo->prepare("SELECT * FROM " . PREFIX_TABLE . "categorie JOIN " . PREFIX_TABLE . "evenement ON " . PREFIX_TABLE . "categorie.cateId = " . PREFIX_TABLE . "evenement.cateId WHERE " . PREFIX_TABLE . "evenement.cateId = " . PREFIX_TABLE . "categorie.cateId AND " . PREFIX_TABLE . "categorie.categorieOriginale = 1");
         $stmt->execute();
@@ -114,7 +199,12 @@ class CategorieDao {
     }
 
 
-    // recupere tout des categories appartenant a la categorie originale culture pour chaque evenement (pour la page categorie)
+    /**
+     * function findCateOriCultureEvt
+     * @brief Fonction permettant de récupérer tout des catégories appartenant à la catégorie originale culture pour chaque evenement
+     * @details Cette fonction permet de récupérer tout des catégories appartenant à la catégorie originale culture pour chaque evenement
+     * @return array
+     */
     public function findCateOriCultureEvt(): array {
         $stmt = $this->pdo->prepare("SELECT * FROM " . PREFIX_TABLE . "categorie JOIN " . PREFIX_TABLE . "evenement ON " . PREFIX_TABLE . "categorie.cateId = " . PREFIX_TABLE . "evenement.cateId WHERE " . PREFIX_TABLE . "evenement.cateId = " . PREFIX_TABLE . "categorie.cateId AND " . PREFIX_TABLE . "categorie.categorieOriginale = 2");
         $stmt->execute();
@@ -125,7 +215,12 @@ class CategorieDao {
     }
 
 
-    // recupere tout des categories appartenant a la categorie originale sport pour chaque actualite (pour la page categorie)
+    /**
+     * function findCateOriSportActu
+     * @brief Fonction permettant de récupérer tout des catégories appartenant à la catégorie originale sport pour chaque actualité
+     * @details Cette fonction permet de récupérer tout des catégories appartenant à la catégorie originale sport pour chaque actualité
+     * @return array
+     */
     public function findCateOriSportActu(): array {
         $stmt = $this->pdo->prepare("SELECT * FROM " . PREFIX_TABLE . "categorie JOIN " . PREFIX_TABLE . "actualite ON " . PREFIX_TABLE . "categorie.cateId = " . PREFIX_TABLE . "actualite.cateId WHERE " . PREFIX_TABLE . "actualite.cateId = " . PREFIX_TABLE . "categorie.cateId AND" . PREFIX_TABLE . "categorie.categorieOriginale = 1");
         $stmt->execute();
@@ -136,7 +231,13 @@ class CategorieDao {
     }
 
 
-    // recupere tout des categories appartenant a la categorie originale culture pour chaque actualite (pour la page categorie)
+    // récupere tout des catégories appartenant a la catégorie originale culture pour chaque actualité
+    /**
+     * function findCateOriCultureActu
+     * @brief Fonction permettant de récupérer tout des catégories appartenant a la catégorie originale culture pour chaque actualité
+     * @details Cette fonction permet de récupérer tout des catégories appartenant a la catégorie originale culture pour chaque actualité
+     * @return array
+     */
     public function findCateOriCultureActu(): array {
         $stmt = $this->pdo->prepare("SELECT * FROM " . PREFIX_TABLE . "categorie JOIN " . PREFIX_TABLE . "actualite ON " . PREFIX_TABLE . "categorie.cateId = " . PREFIX_TABLE . "actualite.cateId WHERE " . PREFIX_TABLE . "actualite.cateId = " . PREFIX_TABLE . "categorie.cateId AND" . PREFIX_TABLE . "categorie.categorieOriginale = 2");
         $stmt->execute();
