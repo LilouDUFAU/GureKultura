a2ac0dff250383badf398fc420be2ce635bbc73f
Lilou DUFAU <ldufau007@iutbayonne.univ-pau.fr>
2025-01-21 17:55:06 +0100

    suppression du render inutile

--- app/controllers/controller_propEv.class.php
+++ app/controllers/controller_propEv.class.php
@@ -217,13 +217,6 @@ class ControllerPropEv extends Controller
 
             ]);
         } else {
-            // echo $this->getTwig()->render('propEv.html.twig', [
-            //     'title' => 'Proposition d\'événement',
-            //     'donnees' => $donnees,
-            //     'actualites' => $actualite,
-            //     'categories' => $categories
-            
-            // ]);
             
             
             header('Location: index.php?controlleur=index&methode=lister');
@@ -269,6 +262,7 @@ class ControllerPropEv extends Controller
                 new DateTime($donnees['finHeure']),
                 $donnees['lieu'],
                 $donnees['photo'] ?? null,
+                false,
                 $donnees['userId'],
                 $donnees['cateId']
             );
