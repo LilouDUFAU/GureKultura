fb638ba3c02ee6024b71aaa839b8a6b3b63f1ffb
EtienneDumai <etienne.dumai@gmail.com>
2025-01-25 00:11:03 +0100

    feature-gestion-d'echec-d'insertion-dans-la-bd-géré-pour-propEv

--- app/controllers/controller_propEv.class.php
+++ app/controllers/controller_propEv.class.php
@@ -36,7 +36,7 @@ class ControllerPropEv extends Controller
      */
     public function lister()
     {
-        $pdo = Bd::getInstance()->getPdo();
+        
 
         $loader = new \Twig\Loader\FilesystemLoader('../templates');
         $twig = new \Twig\Environment($loader);
@@ -73,161 +73,172 @@ class ControllerPropEv extends Controller
 
         // verifier si l'utilisateur est connecté
         if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+
+
+            // definition des regles de validations que l'on souhaite verifier pour chaque champs du formulaire
+            $regleValidation = [
+                'titre' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurMin' => 5,
+                    'longueurMax' => 30,
+                    'format' => '/^[a-zA-Z0-9\s]+$/'
+                ],
+                'cateId' => [
+                    'obligatoire' => true,
+                    'type' => 'integer',
+                    'longueurMin' => 1,
+                    'longueurMax' => 100,
+                    'format' => '/^[a-zA-Z0-9\s]+$/'
+                ],
+                'autorisation' => [
+                    'obligatoire' => false,
+                    'type' => '.pdf ,.jpg, .jpeg, .png',
+                    'format' => '/^[a-zA-Z0-9\s]+$/'
+                ],
+                'email' => [
+                    'obligatoire' => true,
+                    'type' => 'email',
+                    'longueurMin' => 10,
+                    'longueurMax' => 100,
+                    'format' => FILTER_VALIDATE_EMAIL
+                ],
+                'tel' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurMin' => 10,
+                    'longueurMax' => 14,
+                    'format' => '^0\d(\s|-)?(\d{2}(\s|-)?){4}$'
+                ],
+                'nomRep' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurMin' => 2,
+                    'longueurMax' => 100,
+                    'format' => '/^[a-zA-Z0-9\s]+$/'
+                ],
+                'prenomRep' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurMin' => 2,
+                    'longueurMax' => 100,
+                    'format' => '/^[a-zA-Z0-9\s]+$/'
+                ],
+                'description' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurMin' => 30,
+                    'longueurMax' => 500,
+                    'format' => '/^[a-zA-Z0-9\s]+$/'
+                ],
+                'debutDate' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurExacte' => 10,
+                    'format' => '/^\d{4}-\d{2}-\d{2}$/'
+                ],
+                'finDate' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurExacte' => 10,
+                    'format' => '/^\d{4}-\d{2}-\d{2}$/'
+                ],
+                'debutHeure' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurExacte' => 5,
+                    'format' => '/^\d{2}:\d{2}$/'
+                ],
+                'finHeure' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurExacte' => 5,
+                    'format' => '/^\d{2}:\d{2}$/'
+                ],
+                'lieu' => [
+                    'obligatoire' => true,
+                    'type' => 'string',
+                    'longueurMin' => 5,
+                    'longueurMax' => 100,
+                    'format' => '/^[a-zA-Z0-9\s]+$/'
+                ],
+                'photo' => [
+                    'obligatoire' => false,
+                    'type' => 'string',
+                    'longueurMin' => 5,
+                    'longueurMax' => 100,
+                    'format' => '/^[a-zA-Z0-9\s]+$/'
+                ]
+            ];
+
+            // instanciation de la classe de validation
+            $validator = new Validator($regleValidation);
+
+            // recuperation des donnees du formulaire
+            $donnees = $_POST;
             
-       
-        // definition des regles de validations que l'on souhaite verifier pour chaque champs du formulaire
-        $regleValidation = [
-            'titre' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurMin' => 5,
-                'longueurMax' => 30,
-                'format' => '/^[a-zA-Z0-9\s]+$/'
-            ],
-            'cateId' => [
-                'obligatoire' => true,
-                'type' => 'integer',
-                'longueurMin' => 1,
-                'longueurMax' => 100,
-                'format' => '/^[a-zA-Z0-9\s]+$/'
-            ],
-            'autorisation' => [
-                'obligatoire' => false,
-                'type' => '.pdf ,.jpg, .jpeg, .png',
-                'format' => '/^[a-zA-Z0-9\s]+$/'
-            ],
-            'email' => [
-                'obligatoire' => true,
-                'type' => 'email',
-                'longueurMin' => 10,
-                'longueurMax' => 100,
-                'format' => FILTER_VALIDATE_EMAIL
-            ],
-            'tel' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurMin' => 10,
-                'longueurMax' => 14,
-                'format' => '^0\d(\s|-)?(\d{2}(\s|-)?){4}$'
-            ],
-            'nomRep' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurMin' => 2,
-                'longueurMax' => 100,
-                'format' => '/^[a-zA-Z0-9\s]+$/'
-            ],
-            'prenomRep' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurMin' => 2,
-                'longueurMax' => 100,
-                'format' => '/^[a-zA-Z0-9\s]+$/'
-            ],
-            'description' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurMin' => 30,
-                'longueurMax' => 500,
-                'format' => '/^[a-zA-Z0-9\s]+$/'
-            ],
-            'debutDate' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurExacte' => 10,
-                'format' => '/^\d{4}-\d{2}-\d{2}$/'
-            ],
-            'finDate' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurExacte' => 10,
-                'format' => '/^\d{4}-\d{2}-\d{2}$/'
-            ],
-            'debutHeure' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurExacte' => 5,
-                'format' => '/^\d{2}:\d{2}$/'
-            ],
-            'finHeure' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurExacte' => 5,
-                'format' => '/^\d{2}:\d{2}$/'
-            ],
-            'lieu' => [
-                'obligatoire' => true,
-                'type' => 'string',
-                'longueurMin' => 5,
-                'longueurMax' => 100,
-                'format' => '/^[a-zA-Z0-9\s]+$/'
-            ],
-            'photo' => [
-                'obligatoire' => false,
-                'type' => 'string',
-                'longueurMin' => 5,
-                'longueurMax' => 100,
-                'format' => '/^[a-zA-Z0-9\s]+$/'
-            ]
-        ];
-
-        // instanciation de la classe de validation
-        $validator = new Validator($regleValidation);
-
-        // recuperation des donnees du formulaire
-        $donnees = $_POST;
-        var_dump($donnees);
-        // boucle de nettoyage des donnees
-        foreach ($donnees as $key => $value) {
-            $donnees[$key] = htmlentities($value);
-        }
-        $user=$_SESSION['user'];
-        $donnees['userId'] = $user->getUserId();
+            // boucle de nettoyage des donnees
+            foreach ($donnees as $key => $value) {
+                $donnees[$key] = htmlentities($value);
+            }
+            $user = $_SESSION['user'];
+            $donnees['userId'] = $user->getUserId();
 
 
-        // validation des donnees du formulaire
-        $donneesValides = $validator->valider($donnees);
+            // validation des donnees du formulaire
+            $donneesValides = $validator->valider($donnees);
 
-        if(!$donneesValides){
-            $messageErreurs = $validator->getMessageErreurs();
-        }
+            if (!$donneesValides) {
+                $messageErreurs = $validator->getMessageErreurs();
+            }
 
-        // recuperation des erreurs
+            // recuperation des erreurs
 
-        // Rendre le template Twig
-        $pdo = Bd::getInstance()->getPdo();
+            // Rendre le template Twig
 
-        $loader = new \Twig\Loader\FilesystemLoader('../templates');
-        $twig = new \Twig\Environment($loader);
+            $loader = new \Twig\Loader\FilesystemLoader('../templates');
+            $twig = new \Twig\Environment($loader);
 
-        $managerActualite = new ActualiteDao($this->getPdo());
-        $actualite = $managerActualite->findAllWithCategorie();
+            $managerActualite = new ActualiteDao($this->getPdo());
+            $actualite = $managerActualite->findAllWithCategorie();
 
-        $managerCategorie = new CategorieDao($this->getPdo());
-        $categories = $managerCategorie->findAll();
+            $managerCategorie = new CategorieDao($this->getPdo());
+            $categories = $managerCategorie->findAll();
 
-        if (!empty($messageErreurs)) {
-            // Les données ne sont pas valides, affichez les erreurs
+            if (!empty($messageErreurs)) {
+                // Les données ne sont pas valides, affichez les erreurs
+                echo $this->getTwig()->render('propEv.html.twig', [
+                    'title' => 'Proposition d\'événement',
+                    'messageErreurs' => $messageErreurs,
+                    'donnees' => $donnees,
+                    'actualites' => $actualite,
+                    'categories' => $categories
+
+                ]);
+            } else {
+                if (!$this->insererDonneesDansLaBase($donnees)) {
+                    echo $this->getTwig()->render('propEv.html.twig', [
+                        'title' => 'Proposition d\'événement',
+                        'erreurBD' => "Erreur lors de l'insertion de l'événement",
+                        'donnees' => $donnees,
+                        'actualites' => $actualite,
+                        'categories' => $categories
+                    ]);
+                    exit();
+                }
+                header('Location: index.php?controlleur=index&methode=lister');
+                // Les données sont valides, insérez-les dans la base de données
+
+
+            }
+        } else {
             echo $this->getTwig()->render('propEv.html.twig', [
                 'title' => 'Proposition d\'événement',
-                'messageErreurs' => $messageErreurs,
-                'donnees' => $donnees,
-                'actualites' => $actualite,
-                'categories' => $categories
-
+                'messageErreurs' => "Vous devez vous connecter pour proposer un événement"
             ]);
-        } else {
-            if(!$this->insererDonneesDansLaBase($donnees)){
-                echo '<script>alert("Erreur lors de l\'insertion des données dans la base de données")</script>';
-            }            
-            header('Location: index.php?controlleur=index&methode=lister');
-            // Les données sont valides, insérez-les dans la base de données
-             
-
         }
-        } 
     }
-    
+
 
     /**
      * @function insererDonneesDansLaBase
@@ -242,9 +253,9 @@ class ControllerPropEv extends Controller
     private function insererDonneesDansLaBase(array $donnees): bool
     {
         try {
-            $pdo = Bd::getInstance()->getPdo();
-            $managerEvenement = new EvenementDao($pdo);
-    
+            
+            $managerEvenement = new EvenementDao($this->getPdo());
+
             // Créez un nouvel objet Evenement avec les données du formulaire
             $evenement = new Evenement(
                 null,
@@ -265,19 +276,15 @@ class ControllerPropEv extends Controller
                 $donnees['userId'],
                 $donnees['cateId']
             );
-    
-            // Log the event data for debugging
-            error_log(print_r($evenement, true));
-    
             // Insérez l'événement dans la base de données
             $managerEvenement->insert($evenement);
             return true;
         } catch (Exception $e) {
             // Log the error message
             error_log("Error inserting event: " . $e->getMessage());
-            error_log("Je vais retourner false");
             return false;
-            
+
         }
+        
     }
 }
