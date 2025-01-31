9e6768adbdeb87ca47696d3b5daaf360816a654b
Theo LAGUE <tlague@iutbayonne.univ-pau.fr>
2025-01-31 12:21:29 +0100

    Derniere version du projet avec l'ajout de commentaire

--- php/include.php
+++ php/include.php
@@ -29,12 +29,8 @@ require_once '../app/controllers/controller_mesActu.class.php';
 require_once '../app/controllers/controller_modifEv.class.php';
 require_once '../app/controllers/controller_modifActu.class.php';
 require_once '../app/controllers/controller_validPropEv.class.php';
-<<<<<<< HEAD
-require_once '../app/controllers/controller_filtre.class.php';
-
-=======
 require_once '../app/controllers/controller_cateEvt.class.php';
->>>>>>> origin
+require_once '../app/controllers/controller_filtre.class.php';
 
 
 
