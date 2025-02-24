bd35c2718ebb4c991d35233e026c42b1675f8c93
Kyllian Morance <morance.kyllian@gmail.com>
2025-02-24 17:12:20 +0100

    ajout de nouvelles verifications

--- js/erreurSaisie.js
+++ js/erreurSaisie.js
@@ -1,72 +1,28 @@
 function verifErreurSaisie(input) {
     var valeur = input.value.trim();
     var type = input.type;
-    var iconErreur = document.createElement("img");
-
-    iconErreur.src = "../asset/icones/eye.svg";
-    iconErreur.alt = "Erreur de saisie";
-    iconErreur.className = "absolute right-2 top-1/2 transform -translate-y-1/2 w-6 h-6 py-1 rounded-md";
-    iconErreur.id = "iconErreur";
-
-    
-    var messageErreur = document.createElement("p");
-
-    messageErreur.className = "absolute right-2 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-button py-1 rounded-md";
-    messageErreur.id = "messageErreur";
-    
-    var divParent = input.parentElement.childNodes;
-    var exist = false;
-
     // Vérification des champs en fonction du type
     switch (type) {
-        case "text":
-            if (valeur === "") {
-                console.log("text");
-                //<img src="../asset/icones/eye.svg" class="absolute right-2 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-button py-1 rounded-md" alt="Afficher le mot de passe">
-                input.classList.remove("border-white");
-                input.classList.add("border-[#A23145]");
+        case "text": // Zone de texte
+            if (input.id === "titre" && (valeur === "" || valeur.length > 50)) {
+                addError(input, "Les titres ne peuvent pas être vides ou dépasser 50 caractères !");
 
-                divParent.forEach(enfantDiv => {
-                    if(enfantDiv.id == "iconErreur"){
-                        exist = true;
-                    }
-                });
-                if(!exist){
-                    input.messageErreur.value = "messageErreur";
-                    input.parentElement.appendChild(iconErreur);
-                    input.parentElement.appendChild(messageErreur);
-                }
+            } else if (input.id === "description" && (valeur === "" || valeur.length > 50)) {
+                addError(input, "Les titres ne peuvent pas être vides ou dépasser 50 caractères !");
 
-            }else{
-                document.getElementById("iconErreur").remove();
-                input.classList.add("border-white");
-                input.classList.remove("border-[#A23145]");
+            } else {
+                removeError(input);
             }
+            break;
 
         case "email":
             console.log("email");
             var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
             if (!emailPattern.test(valeur)) {
-                //<img src="../asset/icones/eye.svg" class="absolute right-2 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-button py-1 rounded-md" alt="Afficher le mot de passe">
-                input.classList.remove("border-white");
-                input.classList.add("border-[#A23145]");
+                addError(input, "veuilez entré un email valide ! mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
 
-                divParent.forEach(enfantDiv => {
-                    if(enfantDiv.id == "iconErreur"){little44
-                        exist = true;
-                    }
-                });
-                if(!exist){
-                    messageErreur.value = "messageErreur";
-                    console.log(messageErreur.value);
-                    input.parentElement.appendChild(iconErreur);
-                    input.parentElement.appendChild(messageErreur);
-                }
-
-            }else{
-                document.getElementById("iconErreur").remove();
-                input.classList.add("border-white");
-                input.classList.remove("border-[#A23145]");
+            } else {
+                removeError(input);
             }
             break;
 
@@ -74,26 +30,63 @@ function verifErreurSaisie(input) {
             console.log("tel");
             var nombre = parseInt(valeur, 10);
             if (isNaN(nombre) || valeur.length != 10 || valeur[0] != 0) {
-                input.classList.remove("border-white");
-                input.classList.add("border-[#A23145]");
+                addError(input, "veuillez entrer un numéro de téléphone valide !");
 
-                divParent.forEach(enfantDiv => {
-                    if(enfantDiv.id == "iconErreur"){
-                        exist = true;
-                    }
-                });
-                if(!exist){
-                    input.parentElement.appendChild(iconErreur);
-                }
-
-            }else{
-                document.getElementById("iconErreur").remove();
-                input.classList.add("border-white");
-                input.classList.remove("border-[#A23145]");
+            } else {
+                removeError(input);
             }
             break;
 
         default:
             break;
     }
+}
+
+function removeError(input) {
+    var divParent = input.parentElement.childNodes;
+    var exist = false;
+    divParent.forEach(enfantDiv => {
+        if (enfantDiv.id == "iconErreur" || enfantDiv.id == "messageErreur") {
+            exist = true;
+        }
+    });
+    if (exist) {
+        document.getElementById("iconErreur").remove();
+        document.getElementById("messageErreur").remove();
+        input.classList.add("border-white");
+        input.classList.remove("border-error");
+    }
+}
+
+function addError(input, msgError) {
+
+    var divParent = input.parentElement.childNodes;
+    var exist = false;
+
+    var iconErreur = document.createElement("img");
+
+    iconErreur.src = "../asset/icones/eye.svg";
+    iconErreur.alt = "Erreur de saisie";
+    iconErreur.className = "absolute right-2 top-1/2 transform -translate-y-1/2 w-6 h-6 py-1 rounded-md";
+    iconErreur.id = "iconErreur";
+
+    var messageErreur = document.createElement("p");
+
+    messageErreur.className = "row-start-3 absolute w-full h-6 py-1 text-error";
+    messageErreur.id = "messageErreur";
+
+    divParent.forEach(enfantDiv => {
+        if (enfantDiv.id == "iconErreur" || enfantDiv.id == "messageErreur") {
+            exist = true;
+        }
+    });
+    if (!exist) {
+
+        input.classList.remove("border-white");
+        input.classList.add("border-error");
+        messageErreur.innerHTML = msgError;
+        input.parentElement.appendChild(iconErreur);
+        input.parentElement.appendChild(messageErreur);
+    }
+
 }
